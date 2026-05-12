33ab5f052f74e0ad42a588bfd9c453adff60353c
Rohan <rk2024@duck.com>
2026-05-12 12:36:28 +0530

    chore

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ app.use(express.json());
 const cors = require("cors");
 
 const corsOptions = {
-origin: ["https://meet-up-frontend-ivory.vercel.app", "http://localhost:5173"], 
-    methods: ["GET", "POST", "PUT", "DELETE"],  credentials: true,
+origin: "*",
+credentials: true,
   optionSuccessStatus: 200,
 };
 
